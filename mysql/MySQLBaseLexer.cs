41e6b961b4c45cebd9eaa4e70d8e2fe542985276
Ken Domino <kenneth.domino@domemtech.com>
2020-01-12 09:04:06 -0500

    Add prelim version of mysql.

---
+++ mysql/MySQLBaseLexer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.IO;
+using System.Text;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Atn;
+using Antlr4.Runtime.Misc;
+
+namespace mysql
+{
+    public class MySQLBaseLexer : Lexer, IMySQLRecognizerCommon
+    {
+        public MySQLBaseLexer(ICharStream input, TextWriter output, TextWriter errorOutput) 
+            : base(input, output, errorOutput)
+        { }
+
+        public override string[] RuleNames => throw new NotImplementedException();
+
+        public override IVocabulary Vocabulary => throw new NotImplementedException();
+
+        public override string GrammarFileName => throw new NotImplementedException();
+    }
+}
---
+++ mysql/Program.cs
@@ -0,0 +1,34 @@
+﻿using Antlr4.Runtime;
+
+namespace mysql
+{
+    public class Program
+    {
+        static void Try(string input)
+        {
+            var str = new AntlrInputStream(input);
+            var lexer = new MySQLLexer(str);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new MySQLParser(tokens);
+            var listener = new ErrorListener<IToken>(parser, lexer, tokens);
+            parser.AddErrorListener(listener);
+            var tree = parser.query();
+            if (listener.had_error)
+            {
+                System.Console.WriteLine("error in parse.");
+            }
+            else
+            {
+                System.Console.WriteLine("parse completed.");
+                System.Console.WriteLine(tokens.OutputTokens());
+                System.Console.WriteLine(tree.OutputTree(tokens));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Try("1 + 2 * 3");
+            Try("1 + 2 3 * ");
+        }
+    }
+}
