699647e42734e13ab072e30d04d4b0c82ea61b3f
Ken Domino <kenneth.domino@domemtech.com>
2020-01-14 19:56:33 -0500

    Updates, readme.

--- mysql/Program.cs
+++ mysql/Program.cs
@@ -163,18 +163,43 @@ WHERE
             Try(@"DELETE FROM category 
 WHERE
     id = 8;");
-
-            // Negative (does not parse) tests.
-            Try(@"select 2 as expected, /*!01000/**/*/ 2 as result");
             Try("select col1 from foobar where col1 > 1 and col1 < 10;");
             Try(@"select (select t1.id as a, sakila.actor.actor_id b, t2.id c, (select  1 * 0.123, a from t3) from  `ÄÖÜ丈` t1, sakila.actor as t2
-where ((t1.id = t2.id)) and (t1.id = sakila.actor.actor_id)) as r1, 2");
+where ((t1.id = t2.id)) and (t1.id = sakila.actor.actor_id)) as r1, 2;");
             Try(@"select 1 from
   {OJ d left outer join e on a = b} left outer join ee on aa = bb,
   a t1 force index for order by (a, b),
   (select 2 from zz) yy straight_join (select 3) as xx on ww,
   (b, c),
-  (f, g) inner join h using (aa, bb),(h) straight_join `schema`.`table` on yy < zz natural right join ({OJ i left outer join j on ii = jj})");
+  (f, g) inner join h using (aa, bb),(h) straight_join `schema`.`table` on yy < zz natural right join ({OJ i left outer join j on ii = jj});");
+            Try(@"select A, B, A OR B, A XOR B, A AND B from t1_30237_bool where C is null order by A, B;");
+            Try(@"select count(distinct a.actor_id), phone, first_name, a.last_name, country.country 
+from sakila.actor a, address aa, country
+where (a.actor_id = 0 and country_id > 0) 
+group by actor_id;");
+            Try(@"drop user current_user(), 'mike'@localhost;");
+            Try(@"CREATE definer = `root`@`localhost` trigger `upd_film` AFTER UPDATE ON `film` FOR EACH ROW BEGIN
+  IF (old.title != new.title) or (old.description != new.description)
+  THEN
+    UPDATE film_text
+      SET title=new.title,
+          description=new.description,
+          film_id=new.film_id
+    WHERE film_id=old.film_id;
+  END IF;
+END;");
+            Try(@"CREATE TABLE total (
+  a INT NOT NULL AUTO_INCREMENT,
+  message CHAR(20), INDEX(a))
+  ENGINE=MERGE UNION=(t1,t2) INSERT_METHOD=LAST;
+");
+            Try(@"SELECT a FROM tick t WHERE timestamp > (((((((SELECT 1)  + 1))))));");
+            Try(@"select * from (select 1 from dual);");
+
+
+
+            // Negative (does not parse) tests.
+            Try(@"select 2 as expected, /*!01000/**/*/ 2 as result;");
             Try(@"CREATE PROCEDURE do_insert(value INT)
   BEGIN
     -- declare variables to hold diagnostics area information
@@ -199,31 +224,8 @@ where ((t1.id = t2.id)) and (t1.id = sakila.actor.actor_id)) as r1, 2");
   END IF;
   -- say what happened
   SELECT result;
-END");
-            Try(@"grant alter (a, b) on table * to 'mike'@'%'");
-            Try(@"select A, B, A OR B, A XOR B, A AND B from t1_30237_bool where C is null order by A, B");
-            Try(@"select count(distinct a.actor_id), phone, first_name, a.last_name, country.country 
-from sakila.actor a, address aa, country
-where (a.actor_id = 0 and country_id > 0) 
-group by actor_id");
-            Try(@"drop user current_user(), 'mike'@localhost");
-            Try(@"CREATE definer = `root`@`localhost` trigger `upd_film` AFTER UPDATE ON `film` FOR EACH ROW BEGIN
-  IF (old.title != new.title) or (old.description != new.description)
-  THEN
-    UPDATE film_text
-      SET title=new.title,
-          description=new.description,
-          film_id=new.film_id
-    WHERE film_id=old.film_id;
-  END IF;
-END");
-            Try(@"CREATE TABLE total (
-  a INT NOT NULL AUTO_INCREMENT,
-  message CHAR(20), INDEX(a))
-  ENGINE=MERGE UNION=(t1,t2) INSERT_METHOD=LAST;
-");
-            Try(@"SELECT a FROM tick t WHERE timestamp > (((((((SELECT 1)  + 1))))))");
-            Try(@"select * from (select 1 from dual)");
+END;");
+            Try(@"grant alter (a, b) on table * to 'mike'@'%';");
             Try(@"ALTER USER u@localhost IDENTIFIED WITH sha256_password BY 'test';");
 
 
