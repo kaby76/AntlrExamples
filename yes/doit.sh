a4ade2a26e1a3a2f8026ff881d1ae702a5c7b9e7
Ken Domino <kenneth.domino@domemtech.com>
2020-05-04 10:45:51 -0400

    Update with a smaller input for "yes".

--- yes/doit.sh
+++ yes/doit.sh
@@ -11,3 +11,15 @@ time (yes yes | head -10000000 | ./yes2/bin/Debug/netcoreapp3.1/yes2.exe )
 echo yes3
 cd yes3; rm -rf bin obj; dotnet build; sed -n '6,8p' Yes3.g4; cd ..
 time (yes yes | head -10000000 | ./yes3/bin/Debug/netcoreapp3.1/yes3.exe )
+
+echo yes1
+cd yes1; sed -n '6,8p' Yes1.g4; cd ..
+time (yes yes | head -1000 | ./yes1/bin/Debug/netcoreapp3.1/yes1.exe )
+
+echo yes2
+cd yes2; sed -n '6,8p' Yes2.g4; cd ..
+time (yes yes | head -1000 | ./yes2/bin/Debug/netcoreapp3.1/yes2.exe )
+
+echo yes3
+cd yes3; sed -n '6,8p' Yes3.g4; cd ..
+time (yes yes | head -1000 | ./yes3/bin/Debug/netcoreapp3.1/yes3.exe )
