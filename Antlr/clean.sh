0be403e7dd44336ec8b339ec503f20defc211e1a
Ken Domino <kenneth.domino@domemtech.com>
2020-06-10 06:07:21 -0400

    Clean up.

---
+++ Antlr/clean.sh
@@ -0,0 +1,14 @@
+#
+rm o
+rm -rf bin obj
+rm -rf .vs .idea .vscode
+rm -f ANTLRv4BaseListener.cs
+rm -f ANTLRv4BaseVisitor.cs
+rm -f ANTLRv4Lexer.cs
+rm -f ANTLRv4Listener.cs
+rm -f ANTLRv4Parser.cs
+rm -f ANTLRv4Visitor.cs
+rm -f ANTLRv4Lexer.interp
+rm -f ANTLRv4Lexer.tokens
+rm -f ANTLRv4.interp
+rm -f ANTLRv4.tokens
