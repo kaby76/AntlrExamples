c66c3399de71495705f73f9e6196c7334eee750e
Ken Domino <kenneth.domino@domemtech.com>
2020-07-06 08:34:21 -0400

    Adding in "Binary".

---
+++ Binary/BinaryCSharp/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime.Misc;
+
+namespace Binary
+{
+    public class Class1 : BinaryBaseListener
+    {
+        private bool changed = false;
+
+        public override void ExitParExpr(BinaryParser.ParExprContext ctx)
+        {
+            var input = ctx.Start.InputStream;
+
+            if (!changed)
+            {
+                int a = ctx.Start.StartIndex;
+                int b = ctx.Stop.StopIndex;
+
+                var interval = new Interval(a, b);
+                var branch = input.GetText(interval);
+
+                // Simplifying value in "branch"
+                // If "branch" value changes, make "changed" variable true
+            }
+
+            System.Console.WriteLine("Start "
+            + ctx.Start.StartIndex
+            + " Stop "
+            + ctx.Stop.StopIndex
+            + " "
+            + input.GetText(new Interval(ctx.Start.StartIndex, ctx.Stop.StopIndex)));
+
+        }
+    }
+}
---
+++ Binary/BinaryJava/clean.sh
@@ -0,0 +1,13 @@
+#
+rm -rf bin obj
+rm -rf .vs .idea .vscode
+rm -f XPath31BaseListener.cs
+rm -f XPath31BaseVisitor.cs
+rm -f XPath31Lexer.cs
+rm -f XPath31Listener.cs
+rm -f XPath31Parser.cs
+rm -f XPath31Visitor.cs
+rm -f XPath31Lexer.interp
+rm -f XPath31Lexer.tokens
+rm -f XPath31.interp
+rm -f XPath31.tokens
