d482a59fcbfd781fb43f4098890b55e8b8f1d6ba
Ken Domino <kenneth.domino@domemtech.com>
2020-08-24 08:10:29 -0400

    Adding in the arithmetic examples using the standard arithmetic.g4 from grammars-v4.

---
+++ ArithmeticAntlr4Std/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Program
+{
+    using Antlr4.Runtime;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var str = new Antlr4.Runtime.AntlrInputStream("1 + 2");
+            var lexer = new arithmeticLexer(str);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new arithmeticParser(tokens);
+            var tree = parser.file();
+            System.Console.WriteLine(tree.ToStringTree(parser.RuleNames));
+        }
+    }
+}
