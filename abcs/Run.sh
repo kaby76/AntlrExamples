cc96c77064cff1c03d05717c497bd2d2d57a1dd6
Ken Domino <kenneth.domino@domemtech.com>
2020-09-14 10:50:48 -0400

    Update.

--- abcs/Run.sh
+++ abcs/Run.sh
@@ -3,6 +3,7 @@
 rm -rf obj bin
 dotnet build
 yes 'aaaa bbbbbb cccc' | head -139999999 > input
+# Note, on my beefy machine, 19m to parse a 2GB+ file.
 time ./bin/Debug/netcoreapp3.1/a.exe input
 rm -f input
 rm -rf obj bin
